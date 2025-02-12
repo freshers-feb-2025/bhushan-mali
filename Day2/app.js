811990e149a5e7229de5a51be42fcd9d48f18cc5
bhushan45-thinkitive <bhushan.mali@thinkitive.com>
2025-02-12 20:15:35 +0530

    feature 2

--- Day2/app.js
+++ Day2/app.js
@@ -1,1 +1,13 @@
-console.log("hello")
+function add(a, b) {
+    return a + b;
+}
+
+let sum = add;
+
+function average(a, b, fn) {
+    return fn(a, b) / 2;
+}
+
+let result = average(10, 20, sum);
+
+console.log(result);
---
+++ Day2/var.js
@@ -0,0 +1,34 @@
+
+
+// function test(){
+//     let  name="bhushan";
+//     var age=30;
+
+
+//     console.log(name);
+// console.log(age);
+// }
+
+
+// test();
+
+
+
+
+
+
+
+//     let  name="bhushan";
+//     var age=30;
+//  console.log(name);
+// console.log(age);
+
+
+
+
+
+// console.log(name);//hoisted nhi bho
+console.log(age);
+    let  name="bhushan";
+    var age=30;
+    console.log(age);
