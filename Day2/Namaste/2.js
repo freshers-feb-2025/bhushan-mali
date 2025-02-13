57047a7da277e842775a88fdc50edcedacbdd8f3
bhushan45-thinkitive <bhushan.mali@thinkitive.com>
2025-02-13 18:46:58 +0530

    Delete Day2 directory

--- Day2/Namaste/2.js
+++
@@ -1,9 +0,0 @@
-
-getName();
-console.log(x);
-
-
-var x=7;
-function getName(){
-    console.log("Namste javascript");
-}
--- Day2/Namaste/4.js
+++
@@ -1,9 +0,0 @@
-
-getName();
-console.log(x);//not defined
-
-
-// var x=7;
-function getName(){
-    console.log("Namste javascript");
-}
